757a99ffdbf7318ffdb7392b7182c5eb6140e7fc
Jose Valenzuela <valenz74@gmail.com>
2022-05-18 21:21:32 -0500

    Aprendiendo sobre Condicionales en Java

---
+++ src/CondicionalSwicht.java
@@ -0,0 +1,24 @@
+public class CondicionalSwicht {
+    public static void main(String[] args) {
+    int dia = 9;
+    switch (dia) {
+        case 1:
+            System.out.println("Lunes");
+            break;
+        case 2:
+            System.out.println("Martes");
+            break;
+        case 3:
+            System.out.println("Miercoles");
+            break;
+        case 4:
+            System.out.println("Jueves");
+            break;
+        case 5:
+            System.out.println("Viernes");
+            break;
+        default:
+            System.out.println("Dia no valido");
+    }
+    }
+}
